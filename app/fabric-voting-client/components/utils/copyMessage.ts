0d0922c86724148f13a0408a19bd2b3b59d6df68
Farzad Radnia <f.radnia@wallex.net>
2025-04-03 12:36:32 +0330

    add query for user

--- app/fabric-voting-client/components/utils/copyMessage.ts
+++
@@ -1,8 +0,0 @@
-export default function copyMessage(message: string) {
-  const el = document.createElement('textarea');
-  el.value = message;
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
-}
